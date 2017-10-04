f0c8e82c8f31ba7c8594bad2502d8e8b9a5ba034
myunusov <myunusov@luxoft.com>
2017-10-04 13:12:50 +0600

    Add todo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import App from './containers/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './components/App'
 
+const store = createStore( () => {}, {}); 
 
 render(
-<App />,
+        <Provider store={store}>
+            <App />
+        </Provider>,
         document.getElementById('root')
 );
