ceb0b76cb830721680b046889e76e5ca98d2b7d7
Maxim Yunusov <maximyunusov@maxur.org>
2017-10-10 21:56:38 +0600

    Refactoring

--- config/dev.js
+++ config/dev.js
@@ -3,27 +3,30 @@
 /**
  * Development config
  */
-module.exports = function (_path) {
+module.exports = function (PATHS) {
 
     return {
-        context: _path,
+        context: PATHS.target,
         devtool: 'source-map',
 
-        entry: [
-            'react-hot-loader/patch',
-            // activate HMR for React
-            'webpack-dev-server/client?http://localhost:3000',
-            // bundle the client for webpack-dev-server
-            // and connect to the provided endpoint
-            'webpack/hot/only-dev-server',
-        ],
+        entry: {
+            dev: [
+                'react-hot-loader/patch',
+                // activate HMR for React
+                'webpack-dev-server/client?http://localhost:3000',
+                // bundle the client for webpack-dev-server
+                // and connect to the provided endpoint
+                'webpack/hot/only-dev-server',
+            ]
+        },
+
         devServer: {
             host: 'localhost',
             port: 3000,
             compress: true,
-            // respond to 404s with index.html
+// respond to 404s with index.html
             historyApiFallback: true,
-            // enable HMR on the server
+// enable HMR on the server
             hot: true,
         }
     }
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,15 +8,18 @@ const _configs = {
     development: require(__dirname + '/dev')
 };
 
-const _path = path.resolve('build');
+const PATHS = {
+    source: path.resolve('src'),
+    target: path.resolve('build')
+};
 
 const _load = function (environment) {
     if (!environment) throw 'Can\'t find local environment variable via process.env.NODE_ENV';
     if (!_configs[environment]) throw 'Can\'t find environments see _config object';
 
     return _configs && _.merge(
-        _configs[environment](_path),
-        _configs['global'](_path)
+        _configs[environment](PATHS),
+        _configs['global'](PATHS)
     );
 };
 
