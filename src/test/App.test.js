4117844729335434b44dde98a37ca86c28c46731
myunusov <myunusov@luxoft.com>
2017-10-11 16:45:46 +0600

    fix UT

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer';
 import Page from '../main/js/components/Page'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const getPhotos = () => {};
 
 // This is the actual test written for Jest
 it('test to see if the Page renders correctly', () => {
+    let element = <MuiThemeProvider>
+        <Page year={2016} getPhotos={getPhotos} photos={[1, 2, 3, 4, 5]}/>
+    </MuiThemeProvider>;
     const tree = renderer
-            .create(<Page year={2016} getPhotos={getPhotos} photos={[1, 2, 3, 4, 5]}/>)
+            .create(element)
             .toJSON();
     expect(tree).toMatchSnapshot();
 });
